5c12953bfb15223aa97e3b6f08841d97efcb5b40
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-07-31 23:55:23 -0300

    feat: criado a rota para criar assinaturas

--- src/infra/http/routes/subscription/subscriptionRoutes.ts
+++ src/infra/http/routes/subscription/subscriptionRoutes.ts
@@ -1,1 +1,13 @@
-const subscriptionRoutes = Router()
+import { Request, Router } from "express";
+import { SubscriptionController } from "../../controllers/SubscriptionController";
+import { authMiddleware } from "../../middlewares/authMiddleware";
+
+const subscriptionRoutes = Router();
+
+const subscriptionController = new SubscriptionController();
+
+subscriptionRoutes.use(authMiddleware);
+
+subscriptionRoutes.post('subscription', (res, req, next) =>
+    subscriptionController.createSubscription(res, req).catch(next)
+)
