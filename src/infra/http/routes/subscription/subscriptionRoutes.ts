b412215009a98f158e06e1750dd3d1c4689d0054
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-08-04 01:46:38 -0300

    feat: criado a rota /subscriptions

--- src/infra/http/routes/subscription/subscriptionRoutes.ts
+++ src/infra/http/routes/subscription/subscriptionRoutes.ts
@@ -1,6 +1,6 @@
-import { Request, Router } from "express";
-import { SubscriptionController } from "../../controllers/SubscriptionController";
-import { authMiddleware } from "../../middlewares/authMiddleware";
+import { Request, Router } from 'express';
+import { SubscriptionController } from '../../controllers/SubscriptionController';
+import { authMiddleware } from '../../middlewares/authMiddleware';
 
 const subscriptionRoutes = Router();
 
@@ -9,7 +9,11 @@ const subscriptionController = new SubscriptionController();
 subscriptionRoutes.use(authMiddleware);
 
 subscriptionRoutes.post('/subscription', (req, res, next) =>
-    subscriptionController.createSubscription(req, res).catch(next)
-)
+    subscriptionController.createSubscription(req, res).catch(next),
+);
 
-export { subscriptionRoutes }
+subscriptionRoutes.get('/subscriptions', (req, res, next) => 
+    subscriptionController.getAllSubscriptions(req, res).catch(next),
+);
+
+export { subscriptionRoutes };
