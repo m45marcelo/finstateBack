fbf120fd4784951e701af4477b8183c322db6bde
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-08-04 01:23:38 -0300

    feat: criado os repositorios GeneralBudgetRepository e BudgetByCategoryRepository

---
+++ src/application/repositories/BudgetsRepository.ts
@@ -0,0 +1,22 @@
+import { BudgetByCategory, CreateBudgetByCategoryData, CreateGeneralBudgetData, GeneralBudget, UpdateGeneralBudget } from "../../core/entities/Budgets";
+interface GeneralBudgetFilter {
+    iduser: string;
+}
+
+interface BudgetByCategoryFilter {
+    idUser: string;
+}
+
+export interface GeneralBudgetRepository {
+    create(data: CreateGeneralBudgetData): Promise<GeneralBudget>;
+    findMany(filter: GeneralBudgetFilter): Promise<GeneralBudget[]>;
+    update(id: string, data: UpdateGeneralBudget): Promise<GeneralBudget | null>;
+    delete(id: string): Promise<void>;
+}
+
+export interface BudgetByCategoryRepository {
+    create(data: CreateBudgetByCategoryData): Promise<BudgetByCategory>;
+    findMany(filter: BudgetByCategoryFilter): Promise<BudgetByCategory>;
+    update(id: string, data: BudgetByCategoryFilter): Promise<BudgetByCategory | null>
+    delete(id: string): Promise<void>;
+}
