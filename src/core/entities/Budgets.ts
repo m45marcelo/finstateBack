157f61f0e2a83824aa3ef1216083eba78c947e4a
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-08-04 01:25:39 -0300

    feat: criado as entidades GeneralBudget, BudgetByCategory, CreateGeneralBudgetData, CreateBudgetByCategoryData, UpdateGeneralBudget e UpdateBudgetByCategory

---
+++ src/core/entities/Budgets.ts
@@ -0,0 +1,63 @@
+export const BUDGET_STATUS = ['off', 'free', 'exceeded', 'achieved' ] as const;
+export type BudgetStatus = (typeof BUDGET_STATUS)[number];
+
+export const BUDGET_CATEGORIES = [
+    'Supermercado',
+    'Moradia',
+    'Entretenimento',
+    'Transporte',
+    'Lazer',
+    'Saúde',
+    'Vestuário',
+    'Outros',
+] as const;
+export type BudgetCategories = (typeof BUDGET_CATEGORIES)[number];
+
+export interface GeneralBudget {
+    id: string;
+    idUser: string;
+    limit: number;
+    spent: number;
+    remaining: number;
+    status: BudgetStatus;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface BudgetByCategory {
+    id: string;
+    idUser: string;
+    category: BudgetCategories;
+    limit: number;
+    spent: number;
+    remaining: number;
+    status: BudgetStatus;
+    createdAt: Date;
+    updatedAt: Date; 
+}
+
+export interface CreateGeneralBudgetData {
+    idUser: string;
+    category: BudgetCategories;
+    limit: number;
+    spent: number;
+    remaining: number;
+    status: BudgetStatus;
+}
+
+export interface CreateBudgetByCategoryData {
+    idUser: string;
+    category: BudgetCategories;
+    limit: number;
+    spent: number;
+    remaining: number;
+    status: BudgetStatus;
+}
+
+export interface UpdateGeneralBudget {
+    limit?: number;
+}
+
+export interface UpdateBudgetByCategory {
+    limit?: number;
+}
