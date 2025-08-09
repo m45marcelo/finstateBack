451418c5875bc3a37bcf43ae2f7ac59f0eec2fb4
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-08-09 13:21:03 -0300

    feat: criado a logica de atualização de generalBudget ao criar uma expense

--- src/application/use-cases/Expense/CreateExpense.ts
+++ src/application/use-cases/Expense/CreateExpense.ts
@@ -1,7 +1,15 @@
-import { CreateExpenseData, Expense, ExpenseCategory } from "../../../core/entities/Expense";
-import { NotFoundError, ValidationError } from "../../../shared/errors";
-import { ExpenseRepository } from "../../repositories/ExpenseRepository";
-import { UserRepository } from "../../repositories/UserRepository";
+import { BudgetStatus } from '../../../core/entities/Budgets';
+import {
+    CreateExpenseData,
+    Expense,
+    ExpenseCategory,
+} from '../../../core/entities/Expense';
+import { MongoGeneralBudgetRepository } from '../../../infra/repositories/MongoGeneralBudgetRepository';
+import { NotFoundError, ValidationError } from '../../../shared/errors';
+import { GeneralBudgetRepository } from '../../repositories/BudgetsRepository';
+import { ExpenseRepository } from '../../repositories/ExpenseRepository';
+import { UserRepository } from '../../repositories/UserRepository';
+import { UpdateGeneralBudgetUseCase } from '../Budgets/UpdateGeneralBudgetUseCase';
 
 interface CreateExpenseRequest {
     idUser: string;
@@ -11,22 +19,31 @@ interface CreateExpenseRequest {
 }
 
 interface CreateExpenseResponse {
-    expense: Expense
+    expense: Expense;
 }
 
 export class CreateExpenseUseCase {
     constructor(
         private expenseRepository: ExpenseRepository,
-        private userRepository: UserRepository
+        private userRepository: UserRepository,
+        private generalBudgetRepository: GeneralBudgetRepository,
     ) {}
 
-    async execute({ idUser, name, value, category }: CreateExpenseRequest): Promise<CreateExpenseResponse> {
+    async execute({
+        idUser,
+        name,
+        value,
+        category,
+    }: CreateExpenseRequest): Promise<CreateExpenseResponse> {
         const user = await this.userRepository.findById(idUser);
-        if(!user){
+        const generalBudget =
+            await this.generalBudgetRepository.findByIdUser(idUser);
+
+        if (!user) {
             throw new NotFoundError('Usuário não encontrado');
         }
 
-        if(value < 0) {
+        if (value < 0) {
             throw new ValidationError('O valor deve ser maior que zero');
         }
 
@@ -35,12 +52,32 @@ export class CreateExpenseUseCase {
             name: name.trim(),
             value,
             category,
+        };
+
+        if (generalBudget) {
+            const updateGeneralBudgetUseCase = new UpdateGeneralBudgetUseCase(
+                this.generalBudgetRepository,
+            );
+            const spent = generalBudget.spent + value;
+            const remaining = generalBudget.limit - spent;
+            let status: BudgetStatus = 'off';
+
+            if (spent > generalBudget.limit) status = 'exceeded';
+            else if (spent < generalBudget.limit) status = 'free';
+            else status = 'achieved';
+
+            await updateGeneralBudgetUseCase.execute({
+                idUser,
+                spent,
+                remaining,
+                status,
+            });
         }
 
         const expense = await this.expenseRepository.create(expenseData);
 
         return {
-            expense
-        }
+            expense,
+        };
     }
 }
