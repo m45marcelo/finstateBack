d333eda76cdc5c8b0f11322aadae259044124f36
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-08-09 23:57:44 -0300

    feat: criado o caso de uso CreateBudgetByCategoryUseCase

---
+++ src/application/use-cases/Budgets/CreateBudgetByCategory.ts
@@ -0,0 +1,55 @@
+import { BudgetByCategory, BudgetCategories, CreateBudgetByCategoryData } from "../../../core/entities/Budgets";
+import { MongoBudgetByCategoryRepository } from "../../../infra/repositories/MongoBudgetByCategoryRepository";
+import { MongoUserRepository } from "../../../infra/repositories/MongoUserRepository";
+import { ConflictError, NotFoundError } from "../../../shared/errors";
+
+interface CreateBudgetByCategoryRequest {
+    idUser: string;
+    category: BudgetCategories;
+    limit: number;
+}
+
+interface CreateBudgetByCategoryResponse {
+    budgetByCategory: BudgetByCategory;
+}
+
+export class CreateBudgetByCategoryUseCase {
+    constructor(
+                private budgetByCategoryRepository: MongoBudgetByCategoryRepository,
+                private userRepository: MongoUserRepository
+            ) {}
+
+    async execute({ idUser, category, limit }: CreateBudgetByCategoryRequest): Promise<CreateBudgetByCategoryResponse> {
+        const filter = {
+            idUser,
+            category
+        }
+
+        const user = await this.userRepository.findById(idUser);
+
+        if(!user){
+            throw new NotFoundError('Usuário não encontrado');
+        }
+
+        const userHasBudgetInCategory = await this.budgetByCategoryRepository.findMany(filter);
+
+        if(userHasBudgetInCategory.length > 0){
+            throw new ConflictError("Já existe um orçamento feito com essa categoria");
+        }
+
+        const budgetByCategoryData: CreateBudgetByCategoryData = {
+            idUser,
+            category, 
+            limit,
+            spent: 0, 
+            remaining: limit,
+            status: "off",
+        }
+
+        const budgetByCategory = await this.budgetByCategoryRepository.create(budgetByCategoryData)
+
+        return {
+            budgetByCategory
+        }
+    }
+}
