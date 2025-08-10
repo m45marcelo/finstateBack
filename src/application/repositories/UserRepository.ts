c3a7742fd9942c1881e5b78ae0d16bac4ac07814
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-08-10 00:41:41 -0300

    feat: recriado a propriedade findByEmail(email: string): Promise<User | null>

--- src/application/repositories/UserRepository.ts
+++ src/application/repositories/UserRepository.ts
@@ -3,6 +3,7 @@ import { CreatedUserData, UpdateUserData, User } from "../../core/entities/User"
 export interface UserRepository {
     create(data: CreatedUserData): Promise<User>;
     findMany(): Promise<User[]>;
+    findByEmail(email: string): Promise<User | null>
     findById(id: string): Promise<User | null>;
     update(id: string, data: UpdateUserData): Promise<User | null>;
     delete(id: string): Promise<void>;
