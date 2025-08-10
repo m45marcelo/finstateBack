d02cfb1c735109549d14340f92674a404a6526c5
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-08-09 23:59:36 -0300

    feat: criado a rota /budget-by-category

---
+++ src/infra/http/routes/budgets/budgetByCategoryRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { authMiddleware } from '../../middlewares/authMiddleware';
+import { BudgetByCategoryController } from '../../controllers/BudgetByCategoryController';
+
+const budgetByCategoryRoutes = Router();
+const budgetByCategoryController = new BudgetByCategoryController();
+
+budgetByCategoryRoutes.use(authMiddleware);
+
+budgetByCategoryRoutes.post('/budget-by-category', (req, res, next) => {
+    budgetByCategoryController.createBudgetByCategory(req, res).catch(next);
+});
+
+export { budgetByCategoryRoutes };
