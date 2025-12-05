939e274b245ce0b107416a0a46d1ed8bcf0c858f
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-12-04 22:01:45 -0300

    refactor: mudança nos tipos dde data

--- src/application/repositories/BudgetsRepository.ts
+++ src/application/repositories/BudgetsRepository.ts
@@ -1,4 +1,4 @@
-import { BudgetByCategory, BudgetCategories, BudgetStatus, CreateBudgetByCategoryData, CreateGeneralBudgetData, GeneralBudget, UpdateBudgetByCategoryData, UpdateGeneralBudgetData } from "../../core/entities/Budgets";
+import { BudgetByCategory, BudgetCategories, BudgetStatus, CreatedBudgetByCategoryData, CreatedGeneralBudgetData, GeneralBudget, UpdatedBudgetByCategoryData, UpdatedGeneralBudgetData } from "../../core/entities/Budgets";
 
 export interface BudgetByCategoryFilter {
     idUser: string;
@@ -6,16 +6,16 @@ export interface BudgetByCategoryFilter {
 }
 
 export interface GeneralBudgetRepository {
-    create(data: CreateGeneralBudgetData): Promise<GeneralBudget>;
+    create(data: CreatedGeneralBudgetData): Promise<GeneralBudget>;
     findByIdUser(idUser: string): Promise<GeneralBudget | null>;
-    update(id: string, data: UpdateGeneralBudgetData): Promise<GeneralBudget | null>;
+    update(id: string, data: UpdatedGeneralBudgetData): Promise<GeneralBudget | null>;
     delete(id: string): Promise<void>;
 }
 
 export interface BudgetByCategoryRepository {
-    create(data: CreateBudgetByCategoryData): Promise<BudgetByCategory>;
+    create(data: CreatedBudgetByCategoryData): Promise<BudgetByCategory>;
     findMany(filter: BudgetByCategoryFilter): Promise<BudgetByCategory[]>;
     findById(id: string): Promise<BudgetByCategory | null>;
-    update(id: string, data: UpdateBudgetByCategoryData): Promise<BudgetByCategory | null>
+    update(id: string, data: UpdatedBudgetByCategoryData): Promise<BudgetByCategory | null>
     delete(id: string): Promise<void>;
 }
