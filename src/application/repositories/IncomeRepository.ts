7211edb0464eb44cd0f9363e2287b1293e563168
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-12-07 22:42:13 -0300

    refactor mudanças nos tipos de data

--- src/application/repositories/IncomeRepository.ts
+++ src/application/repositories/IncomeRepository.ts
@@ -1,4 +1,4 @@
-import { CreateIncomeData, Income, UpdateIncomeData } from "../../core/entities/Income";
+import { CreatedIncomeData, Income, UpdatedIncomeData } from "../../core/entities/Income";
 
 export interface FindIncomesfilter {
     idUser: string;
@@ -8,10 +8,10 @@ export interface FindIncomesfilter {
 }
 
 export interface IncomeRepository {
-    create(data: CreateIncomeData): Promise<Income>;
+    create(data: CreatedIncomeData): Promise<Income>;
     findById(id: string): Promise<Income | null>;
     findMany(filter: FindIncomesfilter): Promise<Income[]>;
-    update(id: string, data: UpdateIncomeData): Promise<Income | null>;
+    update(id: string, data: UpdatedIncomeData): Promise<Income | null>;
     delete(id: string): Promise<void>;
     getTotalByUser(idUser: string): Promise<number>;
 }
