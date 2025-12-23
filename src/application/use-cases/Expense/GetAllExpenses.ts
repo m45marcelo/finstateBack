0b2a75c88409f05cc57b3c2cc7c0f1a43fdbf922
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-12-22 23:09:49 -0300

    refactor: reformulado

--- src/application/use-cases/Expense/GetAllExpenses.ts
+++ src/application/use-cases/Expense/GetAllExpenses.ts
@@ -1,42 +1,54 @@
 import { Expense, ExpenseCategory } from "../../../core/entities/Expense";
-import { ExpenseRepository, FindExpensesfilter } from "../../repositories/ExpenseRepository";
+import { ExpenseRepository, FindExpensesFilter } from "../../repositories/ExpenseRepository";
 
-interface GetExpensesRequest {
-    idUser: string;
-    category?: ExpenseCategory;
-    startDate?: Date;
-    endDate?: Date;
+interface GetAllExpensesRequest {
+    idUser: string
+    category?: ExpenseCategory
+    startDate?: Date
+    endDate?: Date
+    page?: number
+    limit?: number
 }
 
-interface GetExpensesResponse {
-    expenses: Expense[];
-    total: number;
+interface GetAllExpensesResponse {
+    expenses: Expense[]
+    total: number
+    pagination?: {
+    currentPage: number
+    itemsPerPage: number
+    totalItems: number
+    totalPages: number
+    hasNextPage: boolean
+    hasPreviousPage: boolean
+    }
 }
-
 export class GetAllExpensesUseCase {
     constructor(private expenseRepository: ExpenseRepository) {}
 
-    async execute({ idUser, category, startDate, endDate }: GetExpensesRequest): Promise<GetExpensesResponse> {
+    async execute({ idUser, category, startDate, endDate, page = 1, limit = 10 }: GetAllExpensesRequest): Promise<GetAllExpensesResponse> {
         let fullEndDate: Date | undefined;
 
         if(endDate){
             fullEndDate = new Date(endDate);
             fullEndDate.setUTCHours(23, 59, 59, 999);
         }
-        const filter: FindExpensesfilter = {
+        const filter: FindExpensesFilter = {
             idUser,
             category,
             startDate: startDate? new Date(startDate) : undefined,
             endDate: fullEndDate
         }
 
-        const expenses = await this.expenseRepository.findMany(filter);
-
-        const total = expenses.reduce((sum, expense) => sum + expense.value, 0);
-
+        const result = await this.expenseRepository.findManyPaginated(filter, {
+            page,
+            limit
+        })
+          // Calcular total das despesas da página atual
+        const total = result.data.reduce((sum, expense) => sum + expense.value, 0)
         return {
-            expenses,
-            total
+            expenses: result.data,
+            total,
+            pagination: result.meta
         }
     }
 }
