1a41f654be4a7ab5bf924921baf45f2241aedc54
Marcelo Aug <m45marceloaugusto@gmail.com>
2026-01-11 22:15:25 -0300

    refactory

--- src/application/repositories/UserRepository.ts
+++ src/application/repositories/UserRepository.ts
@@ -1,10 +1,10 @@
-import { CreatedUserData, UpdateUserData, User } from "../../core/entities/User";
+import { CreatedUserData, UpdatedUserData, User } from "../../core/entities/User";
 
 export interface UserRepository {
     create(data: CreatedUserData): Promise<User>;
     findMany(): Promise<User[]>;
     findByEmail(email: string): Promise<User | null>
     findById(id: string): Promise<User | null>;
-    update(id: string, data: UpdateUserData): Promise<User | null>;
+    update(id: string, data: UpdatedUserData): Promise<User | null>;
     delete(id: string): Promise<void>;
 }
