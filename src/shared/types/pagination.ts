b9e125d12a0fe946f0382fd0fbcf0494786a9f61
Marcelo Aug <m45marceloaugusto@gmail.com>
2026-03-09 23:44:28 -0300

    feat: criado as interfaces de Pagination

---
+++ src/shared/types/pagination.ts
@@ -0,0 +1,18 @@
+export interface PaginationParams {
+    page: number
+    limit: number
+  }
+  
+  export interface PaginationMeta {
+    currentPage: number
+    itemsPerPage: number
+    totalItems: number
+    totalPages: number
+    hasNextPage: boolean
+    hasPreviousPage: boolean
+  }
+  
+  export interface PaginatedResponse<T> {
+    data: T[]
+    meta: PaginationMeta
+  }
